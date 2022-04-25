4817e0d607e0a993a33cba9b9be75a0aeba3032d
Luis <38148648+Luis-Jasso@users.noreply.github.com>
2022-04-25 18:58:56 -0500

    codificación de los metodos para el funcionamiento de la app

--- app/src/main/java/com/jasso/gps/MainActivity.kt
+++ app/src/main/java/com/jasso/gps/MainActivity.kt
@@ -1,16 +1,124 @@
 package com.jasso.gps
 
+import android.Manifest
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.location.Address
+import android.location.Geocoder
+import android.location.LocationManager
 import android.os.Bundle
+import android.provider.Settings
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 import com.jasso.gps.databinding.ActivityMainBinding
+import java.util.*
+
+const val REQUEST_CODE_LOCATION = 1
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
+    private lateinit var alertDialog: AlertDialog
+    private lateinit var mFusedLocationProviderClient: FusedLocationProviderClient
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        mFusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
+
+
+    }
+
+
+    private fun isLocationEnabled(): Boolean {
+        val locationManager: LocationManager =
+            getSystemService(Context.LOCATION_SERVICE) as LocationManager
+
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) || locationManager.isProviderEnabled(
+            LocationManager.NETWORK_PROVIDER
+        )
+
+    }
+
+    private fun checkPermission(): Boolean {
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                this, Manifest.permission.ACCESS_FINE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED
+        ) {
+            return true
+        }
+        return false
+    }
+
+    private fun requestPermission() {
+        ActivityCompat.requestPermissions(
+            this, arrayOf(
+                Manifest.permission.ACCESS_COARSE_LOCATION,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ), REQUEST_CODE_LOCATION
+        )
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+            REQUEST_CODE_LOCATION -> {
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    getLocation()
+                }
+            }
+            else -> {
+                Toast.makeText(this, getString(R.string.permission_denied), Toast.LENGTH_SHORT)
+                    .show()
+            }
+        }
+    }
+
+    private fun getLocation() {
+        if (checkPermission()) {
+            // el permiso fue otorgado
+            if (isLocationEnabled()) {
+                //Obtener la ubicacion
+                mFusedLocationProviderClient.lastLocation.addOnCompleteListener(this) { task ->
+                    val location = task.result
+                    if (location != null) {
+                        val geocoder = Geocoder(this, Locale.getDefault())
+                        val list: List<Address> =
+                            geocoder.getFromLocation(location.latitude, location.longitude, 1)
+                    }
+                }
+
+            } else {
+                //la ubicacion debe ser activada, mostrar aviso al usuario
+                alertDialog = AlertDialog.Builder(this)
+                    .setTitle(getString(R.string.title_aviso))
+                    .setMessage(getString(R.string.content_aviso))
+                    .setPositiveButton(getString(R.string.yes)) { _, _ ->
+                        startActivity(Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS))
+                    }
+                    .setNegativeButton(getString(R.string.No)) { dialog, _ ->
+                        dialog.dismiss()
+                    }
+                    .create()
+                alertDialog.show()
+            }
+        } else {
+            requestPermission()
+        }
     }
 }
