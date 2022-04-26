dfc588eb0c503be340ca1d0d29ca01af9d6f2cdb
Luis <38148648+Luis-Jasso@users.noreply.github.com>
2022-04-26 15:40:56 -0500

    codificación de los metodos para el funcionamiento de la app

--- app/src/main/java/com/jasso/gps/MainActivity.kt
+++ app/src/main/java/com/jasso/gps/MainActivity.kt
@@ -6,6 +6,7 @@ import android.content.Intent
 import android.content.pm.PackageManager
 import android.location.Address
 import android.location.Geocoder
+import android.location.Location
 import android.location.LocationManager
 import android.os.Bundle
 import android.provider.Settings
@@ -13,10 +14,13 @@ import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
-import com.google.android.gms.location.FusedLocationProviderClient
-import com.google.android.gms.location.LocationServices
+import com.google.android.gms.location.*
 import com.jasso.gps.databinding.ActivityMainBinding
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import java.util.*
+import java.util.concurrent.TimeUnit
 
 const val REQUEST_CODE_LOCATION = 1
 
@@ -27,6 +31,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var alertDialog: AlertDialog
     private lateinit var mFusedLocationProviderClient: FusedLocationProviderClient
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -35,8 +40,85 @@ class MainActivity : AppCompatActivity() {
         mFusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
 
 
+        binding.button.setOnClickListener {
+            getLocation()
+        }
+
     }
 
+
+    private fun getLocation() {
+        if (checkPermission()) {
+            // el permiso fue otorgado
+            if (isLocationEnabled()) {
+                //Obtener la ubicacion
+                CoroutineScope(Dispatchers.IO).launch {
+
+
+                    mFusedLocationProviderClient.lastLocation.addOnCompleteListener(this@MainActivity) { task ->
+                        val location = task.result
+                        if (location != null) {
+
+                            val geocoder = Geocoder(this@MainActivity, Locale.getDefault())
+                            val list: List<Address> =
+                                geocoder.getFromLocation(location.latitude, location.longitude, 1)
+
+                            runOnUiThread {
+
+                                Toast.makeText(
+                                    this@MainActivity,
+                                    "mostrando datos en la interfaz",
+                                    Toast.LENGTH_SHORT
+                                ).show()
+
+                                binding.tvCoordenadas.text =
+                                    resources.getString(
+                                        R.string.location_text,
+                                        list[0].latitude,
+                                        list[0].longitude,
+                                        location.time
+                                    )
+
+                                binding.tvAddress.text =
+                                    resources.getString(
+                                        R.string.address_text,
+                                        list[0].countryName,
+                                        list[0].adminArea,
+                                        list[0].postalCode,
+                                        list[0].getAddressLine(0)
+                                    )
+                            }
+                        } else {
+                            runOnUiThread {
+                                Toast.makeText(
+                                    this@MainActivity,
+                                    "No se puede obtener la ubicación, hay problemas...",
+                                    Toast.LENGTH_SHORT
+                                ).show()
+                            }
+                        }
+                    }
+                }
+
+
+            } else {
+                //la ubicacion debe ser activada, mostrar aviso al usuario
+                alertDialog = AlertDialog.Builder(this)
+                    .setTitle(getString(R.string.title_aviso))
+                    .setMessage(getString(R.string.content_aviso))
+                    .setPositiveButton(getString(R.string.yes)) { _, _ ->
+                        startActivity(Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS))
+                    }
+                    .setNegativeButton(getString(R.string.No)) { dialog, _ ->
+                        dialog.dismiss()
+                    }
+                    .create()
+                alertDialog.show()
+            }
+        } else {
+            requestPermission()
+        }
+    }
 
     private fun isLocationEnabled(): Boolean {
         val locationManager: LocationManager =
@@ -89,36 +171,4 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    private fun getLocation() {
-        if (checkPermission()) {
-            // el permiso fue otorgado
-            if (isLocationEnabled()) {
-                //Obtener la ubicacion
-                mFusedLocationProviderClient.lastLocation.addOnCompleteListener(this) { task ->
-                    val location = task.result
-                    if (location != null) {
-                        val geocoder = Geocoder(this, Locale.getDefault())
-                        val list: List<Address> =
-                            geocoder.getFromLocation(location.latitude, location.longitude, 1)
-                    }
-                }
-
-            } else {
-                //la ubicacion debe ser activada, mostrar aviso al usuario
-                alertDialog = AlertDialog.Builder(this)
-                    .setTitle(getString(R.string.title_aviso))
-                    .setMessage(getString(R.string.content_aviso))
-                    .setPositiveButton(getString(R.string.yes)) { _, _ ->
-                        startActivity(Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS))
-                    }
-                    .setNegativeButton(getString(R.string.No)) { dialog, _ ->
-                        dialog.dismiss()
-                    }
-                    .create()
-                alertDialog.show()
-            }
-        } else {
-            requestPermission()
-        }
-    }
 }
